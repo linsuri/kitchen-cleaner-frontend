12c9f4c5c06df9d184d91d1bf2a40ccdc859c696
github username <github email address>
2018-10-22 19:53:09 -0400

    https at yummly img in footer

--- src/components/App.js
+++ src/components/App.js
@@ -135,7 +135,7 @@ class App extends Component {
           saveFavorite={this.saveFavorite}
           unsaveFavorite={this.unsaveFavorite}
         />
-        <p className='footer'>Kitchen Cleaner 2018 by Lin Sriuthenchai • Recipe search powered by <a href='https://www.yummly.co/recipes'><img alt='Yummly' src='httpss://static.yummly.co/api-logo.png'/></a></p>
+        <p className='footer'>Kitchen Cleaner 2018 by Lin Sriuthenchai • Recipe search powered by <a href='https://www.yummly.co/recipes'><img alt='Yummly' src='https://static.yummly.co/api-logo.png'/></a></p>
       </div>
     );
   }
